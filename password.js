1890d185b43f004932bc298f96eb1fb004fb3e3a
M Afnan Khadim <afnan.khadim630@gmail.com>
2024-03-03 22:12:54 +0500

    Initial commit

---
+++ password.js
@@ -0,0 +1,43 @@
+const passwordBox = document.getElementById('password');
+
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_=~|}{][></-=";
+
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (lenght > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+
+function copy() {
+    passwordBox.select();
+    document.execCommand("copy");
+
+    const copyButton = document.getElementById('copyButton');
+    const copyIcon = copyButton.querySelector('img');
+
+    copyButton.innerHTML = '<img src="/images/copied.png">Copied Password';
+
+    copyButton.style.backgroundColor = '#006F3B';
+
+    setTimeout(function () {
+        copyButton.innerHTML = '<img src="/images/copy.png">Copy Password';
+        copyButton.style.backgroundColor = '';
+    }, 1000);
+}
